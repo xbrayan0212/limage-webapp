6e6fb86ecd85ce7e1c0aeb23060535864637386f
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-07-17 10:51:34 -0500

    feat: Añadida ruta pública para la página de inicio

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,11 @@ Route::get('/dashboard', function () {
     return view('welcome');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
