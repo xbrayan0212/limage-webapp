83a52bcba8df612b736f2dc6525a9ac47d715aa0
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-07-17 10:25:39 -0500

    feat: Actualizar navegación en layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,6 @@
-<nav x-data="{ open: false }" class="bg-white shadow-md border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-purple-300 shadow-md border-b border-gray-100">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class=" flex justify-between items-center h-20">
+        <div class="flex justify-between items-center h-20">
             <!-- Logo and Main Links -->
             <div class="flex items-center">
                 <div class="shrink-0 flex items-center">
@@ -10,8 +10,8 @@
                 </div>
                 <!-- Links visible only on large screens -->
                 <div class="hidden sm:flex sm:items-center sm:space-x-4 ms-10">
-                    <a href="#" class="text-black hover:text-red-500 transition">Promociones</a>
-                    <a href="#" class="text-black hover:text-red-500 transition">Agendar Cita</a>
+                    <a href="#" class="text-black hover:text-red-500 transition text-sm">Promociones</a>
+                    <a href="#" class="text-black hover:text-red-500 transition text-sm">Agendar Cita</a>
                 </div>
             </div>
             <!-- User Authentication Links -->
@@ -20,49 +20,38 @@
                     @auth
                     <div class="hidden sm:flex sm:items-center sm:ms-6">
                         <x-dropdown align="right" width="48">
-                                <x-slot name="trigger">
-                                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                                <div>{{ Auth::user()->name }}</div>
-
-                                                <div class="ms-1">
-                                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                                        </svg>
-                                                </div>
-                                        </button>
-                                </x-slot>
-
-                                <x-slot name="content">
-                                        <x-dropdown-link :href="route('profile.edit')">
-                                                {{ __('Profile') }}
-                                        </x-dropdown-link>
-
-                                        <!-- Authentication -->
-                                        <form method="POST" action="{{ route('logout') }}">
-                                                @csrf
-
-                                                <x-dropdown-link :href="route('logout')"
-                                                                onclick="event.preventDefault();
-                                                                                        this.closest('form').submit();">
-                                                        {{ __('Log Out') }}
-                                                </x-dropdown-link>
-                                        </form>
-                                </x-slot>
+                            <x-slot name="trigger">
+                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                    <div>{{ Auth::user()->name }}</div>
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
                         </x-dropdown>
-                </div>
-
+                    </div>
                     @else
-                        <a
-                            href="{{ route('login') }}"
-                            class="text-black hover:text-red-500 border border-transparent rounded px-3 py-2 transition hover:border-red-500"
-                        >
+                        <a href="{{ route('login') }}" class="text-black hover:text-red-500 border border-transparent rounded px-3 py-2 transition hover:border-red-500 text-sm">
                             Iniciar Sesión
                         </a>
                         @if (Route::has('register'))
-                            <a
-                                href="{{ route('register') }}"
-                                class="text-black hover:text-red-500 border border-transparent rounded px-3 py-2 transition hover:border-red-500"
-                            >
+                            <a href="{{ route('register') }}" class="text-black hover:text-red-500 border border-transparent rounded px-3 py-2 transition hover:border-red-500 text-sm">
                                 Registro
                             </a>
                         @endif
@@ -81,56 +70,46 @@
         </div>
     </div>
 
-
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-red-500 transition">Promociones</a>
-            <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-red-500 transition">Agendar Cita</a>
+            <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-red-500 transition text-sm">Promociones</a>
+            <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-red-500 transition text-sm">Agendar Cita</a>
         </div>
 
         @if (Route::has('login'))
             @auth
-     <!-- Responsive Settings Options -->
-			<div class="pt-4 pb-1 border-t border-gray-200">
-                <div class="px-4">
-                        <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                        <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                <!-- Responsive Settings Options -->
+                <div class="pt-4 pb-1 border-t border-gray-200">
+                    <div class="px-4">
+                        <div class="font-medium text-base text-gray-800 text-sm">{{ Auth::user()->name }}</div>
+                        <div class="font-medium text-sm text-gray-500 text-xs">{{ Auth::user()->email }}</div>
+                    </div>
                 </div>
             @else
-                <a
-                    href="{{ route('login') }}"
-                    class="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-red-500 border border-transparent transition hover:border-red-500"
-                >
+                <a href="{{ route('login') }}" class="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-red-500 border border-transparent transition hover:border-red-500 text-sm">
                     Iniciar Sesión
                 </a>
                 @if (Route::has('register'))
-                    <a
-                        href="{{ route('register') }}"
-                        class="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-red-500 border border-transparent transition hover:border-red-500"
-                    >
+                    <a href="{{ route('register') }}" class="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-red-500 border border-transparent transition hover:border-red-500 text-sm">
                         Registro
                     </a>
-
                 @endif
             @endauth
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                        {{ __('Profile') }}
-                </x-responsive-nav-link-->
-
+                    {{ __('Profile') }}
+                </x-responsive-nav-link>
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-
-                        <x-responsive-nav-link :href="route('logout')"
-                                        onclick="event.preventDefault();
-                                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                        </x-responsive-nav-link>
+                    @csrf
+                    <x-responsive-nav-link :href="route('logout')"
+                        onclick="event.preventDefault();
+                                this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-responsive-nav-link>
                 </form>
-        </div>
+            </div>
         @endif
     </div>
 </nav>
-
