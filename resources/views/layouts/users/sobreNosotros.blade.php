df27b8cea748b4ef4c0e7b6713fd71e7737be11f
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-07-17 10:25:40 -0500

    feat: Actualizar sobre nosotros en layout

--- resources/views/layouts/users/sobreNosotros.blade.php
+++ resources/views/layouts/users/sobreNosotros.blade.php
@@ -8,13 +8,13 @@
                 ¡Conócenos!
             </h2>
             <p class="text-[clamp(1em,1vw,4em)] leading-10 text-justify">
-                Bienvenido a [Nombre del Salón de Belleza], tu destino premium para el cuidado y embellecimiento personalizado en [ubicación]. En nuestro salón, fusionamos la pasión por la belleza con la excelencia en el servicio para ofrecerte una experiencia única y rejuvenecedora en cada visita.
+                Bienvenido a L´ Image Beauty, tu destino premium para el cuidado y embellecimiento personalizado en Hato Montaña Machetazo. En nuestro salón, fusionamos la pasión por la belleza con la excelencia en el servicio para ofrecerte una experiencia única y rejuvenecedora en cada visita.
 
-                En [Nombre del Salón de Belleza], nos dedicamos a resaltar tu belleza natural a través de un enfoque personalizado. Nuestros expertos estilistas y terapeutas están comprometidos en proporcionarte un servicio de la más alta calidad, utilizando productos de lujo y técnicas innovadoras para garantizar resultados excepcionales.
+                En L´ Image Beauty, nos dedicamos a resaltar tu belleza natural a través de un enfoque personalizado. Nuestros expertos estilistas y terapeutas están comprometidos en proporcionarte un servicio de la más alta calidad, utilizando productos de lujo y técnicas innovadoras para garantizar resultados excepcionales.
             </p>
         </div>
     </div>
     <div class="w-[650px] max-w-full p-5">
-        <img src="https://plus.unsplash.com/premium_photo-1661576866935-6e92c2f2e9f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="sobreNosotros-img" class="w-full max-h-[800px] object-cover">
+        <img src="{{ asset('images/img-sobreN.png') }}">
     </div>
 </section>
