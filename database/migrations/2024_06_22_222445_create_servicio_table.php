92e0419e1fb8f086468687c91b91dc20d208f192
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-07-18 15:44:30 -0500

    feat: Añadida una nueva característica en la tabla de servicios

--- database/migrations/2024_06_22_222445_create_servicio_table.php
+++ database/migrations/2024_06_22_222445_create_servicio_table.php
@@ -6,17 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateServicioTable extends Migration
 {
-	public function up()
-	{
-		Schema::create('servicio', function (Blueprint $table) {
-			$table->id('idServicio');
-			$table->string('tipo_servicio', 50);
-			$table->timestamps();
-		});
-	}
+    public function up()
+    {
+        Schema::create('servicio', function (Blueprint $table) {
+            $table->bigIncrements('idServicio');
+            $table->string('tipo_servicio', 50);
+            $table->string('servicio_detalle', 255)->nullable();
+            $table->timestamps();
+        });
+    }
 
-	public function down()
-	{
-		Schema::dropIfExists('servicio');
-	}
+    public function down()
+    {
+        Schema::dropIfExists('servicio');
+    }
 }
+
