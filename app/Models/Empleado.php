4ccc143d53f18658559b7bd3650e46468803988e
xbrayan0212 <xbrayan.tobii02@gmail.com>
2024-07-18 15:44:33 -0500

    feat: Implementación del modelo de Empleado para la gestión de trabajadoras

---
+++ app/Models/Empleado.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Model
+{
+    use HasFactory;
+
+    protected $table = 'empleados';
+
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'cedula',
+        'especialidad',
+        'telefono',
+        'email',
+        'direccion',
+        'status',
+    ];
+}
